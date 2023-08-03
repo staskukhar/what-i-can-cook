35a15f141de1ae541bf5b6c91b394beb93356935
staskukhar <staskuk2005@gmail.com>
2023-08-03 22:03:07 +0300

    added components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IngredientsSearcherComponent } from './components/ingredients-searcher/ingredients-searcher.component';
+import { MyIngredientsListService } from './services/my-ingredients-list.service';
+import { IngredientsListComponent } from './components/ingredients-list/ingredients-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        IngredientsSearcherComponent,
+        IngredientsListComponent
+    ],
+    providers: [MyIngredientsListService],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+    ],
+    schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+    ]
 })
 export class AppModule { }
