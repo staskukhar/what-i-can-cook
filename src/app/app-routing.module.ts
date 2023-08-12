d6b2089ec987a14bec2fa6f7c1770308f9ac91b9
staskukhar <staskuk2005@gmail.com>
2023-08-12 14:35:06 +0300

    established routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipesSearcherComponent } from './components/recipes-searcher/recipes-searcher.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: 'recipes-searching', pathMatch: 'full' },
+  { path: 'recipes-searching', component: RecipesSearcherComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
