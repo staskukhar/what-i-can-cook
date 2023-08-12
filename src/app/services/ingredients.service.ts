4df54aec57445ce9181308396db61574b22fee00
staskukhar <staskuk2005@gmail.com>
2023-08-12 14:40:01 +0300

    service provides real data from api

--- src/app/services/ingredients.service.ts
+++ src/app/services/ingredients.service.ts
@@ -1,31 +1,45 @@
 import { Injectable } from '@angular/core';
 import { Ingredient } from '../models/Ingredient';
+import { environmet } from 'src/environments/environment';
+import { HttpClient, HttpClientModule, HttpParamsOptions } from '@angular/common/http';
+import { catchError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IngredientsService {
 
-  constructor() { 
+  protected apiKey!: string;
 
+  constructor(protected httpClient: HttpClient) { 
+    this.apiKey = environmet.spoonacularKey;
   }
-  public getIngredients() : Array<Ingredient> {
-    return [
-      {
-        id: 9003,
-        name: 'apple',
-        image: 'apple.jpg'
-      },
-      {
-        id: 9019,
-        name: "applesauce",
-        image: "applesauce.png"
-      },
-      {
-        id: 9016,
-        name: "apple juice",
-        image: "apple-juice.jpg"
-      }
-    ]
+  public getIngredients(query: string): Ingredient[]{
+    let ingredients: Ingredient[] = [];
+    const autocomplateUrl = this.getAutocomplateUrl(query);
+
+    const options = {
+      responseType: 'json' as const,
+    };
+
+    this.httpClient.get<Ingredient[]>(autocomplateUrl, options)
+      .subscribe(
+        data => {
+          data.forEach(ingredient => {
+            ingredients.push(ingredient);
+          });
+        },
+        error => {
+          console.log(error.name);
+        }
+      );
+        
+    return ingredients;
+  }
+  protected getAutocomplateUrl(query: string): string {
+    let autocomplateSearchLink: string = 'https://api.spoonacular.com/food/ingredients/autocomplete';
+    autocomplateSearchLink = autocomplateSearchLink.concat(`?apiKey=${this.apiKey}&query=${query}`);
+    console.log(autocomplateSearchLink);
+    return autocomplateSearchLink;
   }
 }
