fe6fa3912e94e60e54989ce952c5b8e1135b6917
staskukhar <staskuk2005@gmail.com>
2023-08-22 19:13:33 +0300

    component updated

--- src/app/components/recipes-searcher/recipes-searcher.component.ts
+++ src/app/components/recipes-searcher/recipes-searcher.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { MyIngredientsListService } from 'src/app/services/my-ingredients-list.service';
+import { SpoonacularService } from 'src/app/services/spoonacular.service';
+import { Recipe } from 'src/app/models/Recipe';
 
 @Component({
   selector: 'app-recipes-searcher',
@@ -6,5 +10,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipes-searcher.component.css']
 })
 export class RecipesSearcherComponent {
-
+  public recipes!: Recipe[];
+  constructor (protected myIngredientsService: MyIngredientsListService,
+    protected spoonacularService: SpoonacularService) {}
+  public onSearch(): void {
+    var myIngredients = this.myIngredientsService.getIngredients()
+    if(myIngredients) {
+      this.recipes = this.spoonacularService.getRecipesByIngredients(myIngredients);
+    }
+  }
 }
