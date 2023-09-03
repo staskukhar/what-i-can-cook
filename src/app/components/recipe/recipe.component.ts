ae8db336771be1877da8c3d1c14d603015bc4cda
staskukhar <staskuk2005@gmail.com>
2023-09-03 19:09:20 +0300

    adds button for visit recipe origin source

--- src/app/components/recipe/recipe.component.ts
+++ src/app/components/recipe/recipe.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Recipe } from 'src/app/models/Recipe';
+import { RecipeInfo } from 'src/app/models/RecipeInfo';
+import { SpoonacularService } from 'src/app/services/spoonacular.service';
 
 @Component({
   selector: 'app-recipe',
@@ -9,4 +11,20 @@ import { Recipe } from 'src/app/models/Recipe';
 export class RecipeComponent {
   @Input() recipe!: Recipe;
 
+  constructor(private spoonacularService: SpoonacularService) {}
+
+  openOriginalSource(id: number): void {
+    this.spoonacularService.getRecipeLinkById(id)
+      .subscribe(
+        data => {
+          console.log(data);
+          window.open(data, '_blank');
+        },
+        error => {
+          console.log(error.name);
+        }
+        
+      );
+    
+  }
 }
