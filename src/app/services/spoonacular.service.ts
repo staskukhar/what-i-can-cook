fda5fd462859f8aba108ee0ce45d56638fdab583
staskukhar <staskuk2005@gmail.com>
2023-09-03 19:07:03 +0300

    adds new functionality for getting recipe info

--- src/app/services/spoonacular.service.ts
+++ src/app/services/spoonacular.service.ts
@@ -2,8 +2,10 @@ import { Injectable } from '@angular/core';
 import { Ingredient } from '../models/Ingredient';
 import { environmet } from 'src/environments/environment';
 import { HttpClient, HttpClientModule, HttpParamsOptions } from '@angular/common/http';
-import { catchError } from 'rxjs';
+import { Observable} from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { Recipe } from '../models/Recipe';
+import { RecipeInfo } from '../models/RecipeInfo';
 
 @Injectable({
   providedIn: 'root'
@@ -47,13 +49,17 @@ export class SpoonacularService {
     endpointUrl = endpointUrl.concat(`?apiKey=${this.apiKey}&ingredients=${ingredientNames.join(',+')}`);
     return endpointUrl;
   }
+  protected getEndpointForRecipeInfo(id: number): string {
+    let endpointUrl = 'https://api.spoonacular.com/recipes/';
+    endpointUrl = endpointUrl.concat(`${id}/information?apiKey=${this.apiKey}`);
+    return endpointUrl;
+  }
   public getRecipesByIngredients(ingredients: Ingredient[]): Recipe[] {
     let recipes: Recipe[] = [];
 
     let httpGetLink = this.getRecipeByIngredientsUrl(ingredients.map(i => {
       return i.name;
     }));
-    console.log(httpGetLink);
     const options = {
       responseType: 'json' as const,
     };
@@ -68,9 +74,23 @@ export class SpoonacularService {
         error => {
           console.log(error.name);
         }
-      );
-
-    console.log(recipes);    
+      );  
     return recipes;
+  }
+  public getRecipeLinkById(id: number) : Observable<string> {
+    let link!: string;
+
+    let httpGetLink = this.getEndpointForRecipeInfo(id);
+    const options = {
+      responseType: 'json' as const,
+    };
+    return this.httpClient.get<RecipeInfo>(httpGetLink, options)
+      .pipe(
+        map(data => data.sourceUrl),
+        catchError(error => {
+          console.error('Error:', error);
+          throw error; // You can rethrow or return a default value as needed
+        })
+      );  
   }
 }
