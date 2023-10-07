57819a1ee1a22ce85d7d3ba4923422b1b7f8bb8a
staskukhar <staskuk2005@gmail.com>
2023-10-07 20:37:04 +0300

    migrates model to class type

--- WhatICanCookAPI/Models/Configurations/AuthSettings.cs
+++ WhatICanCookAPI/Models/Configurations/AuthSettings.cs
@@ -1,1 +1,8 @@
-public record AuthSettings(string Audience, string Issuer, string IssuerSigningKey, int ExpiresAt) {}
+public class AuthSettings
+{
+    public string Issuer { get; set; } = string.Empty;
+    public string Audience { get; set; } = string.Empty;
+    public string IssuerSigningKey { get; set; } = string.Empty;
+    public int ExpiresAt { get; set; }
+
+}
