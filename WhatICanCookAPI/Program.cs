fe5932a47df479de50d4b236381441b90d3c0297
staskukhar <staskuk2005@gmail.com>
2023-10-07 12:43:36 +0300

    adds changes to middleware related to identity

--- WhatICanCookAPI/Program.cs
+++ WhatICanCookAPI/Program.cs
@@ -1,36 +1,52 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Routing.Tree;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<WICCDBContext>(options => 
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-string signingKey = builder.Configuration.GetValue<string>("AuthSettings:IssuerSigningKey");
+var authSettings = builder.Configuration.GetValue<AuthSettings>("AuthSettings");
 
-builder.Services.AddAuthentication(
-    options => {
+builder.Services.AddAuthentication( options => 
+    {
         options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
         options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
         options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
     }
-).AddJwtBearer(
-    options => {
-        options.TokenValidationParameters = new TokenValidationParameters{
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(signingKey)),
-            ValidateLifetime = true,
-            ValidateAudience = true,
-            ClockSkew = TimeSpan.Zero,
-            ValidateIssuer = true,
-        };
-    }
-);
+    ).AddJwtBearer(
+        options => {
+            options.TokenValidationParameters = new TokenValidationParameters{
+                ValidateIssuerSigningKey = true,
+                ValidateLifetime = true,
+                ValidateAudience = true,
+                ValidateIssuer = true,
+                ClockSkew = TimeSpan.Zero,
+                ValidIssuer = authSettings.Issuer,
+                ValidAudience = authSettings.Audience,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(authSettings.IssuerSigningKey)),
+            };
+        }
+    );
+
+builder.Services.AddIdentityCore<IdentityUser>(options => 
+    {
+        options.SignIn.RequireConfirmedEmail = true;
+        options.Password.RequiredLength = 8;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireUppercase = true;
+    });
 
 var app = builder.Build();
 
