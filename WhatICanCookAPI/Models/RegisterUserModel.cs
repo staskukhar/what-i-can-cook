a30f767159b976298421758c2120e5a5d0113d7f
staskukhar <staskuk2005@gmail.com>
2023-10-07 18:49:48 +0300

    adds model for user registration

---
+++ WhatICanCookAPI/Models/RegisterUserModel.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+public class RegisterUserModel 
+{
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    [Required]
+    public string Surname { get; set; } = string.Empty;
+    [Required]
+    public string Email { get; set; } = string.Empty;
+    [Required]
+    public string Password { get; set; } = string.Empty;
+
+}
