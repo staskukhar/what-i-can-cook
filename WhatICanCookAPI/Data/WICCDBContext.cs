2a462310a778161308c87666705336921452171b
staskukhar <staskuk2005@gmail.com>
2023-10-21 11:48:15 +0300

    adds refresh token table to db context

--- WhatICanCookAPI/Data/WICCDBContext.cs
+++ WhatICanCookAPI/Data/WICCDBContext.cs
@@ -1,8 +1,12 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Net.Http.Headers;
 
 public class WICCDBContext : IdentityDbContext<UserModel>
 {
     public WICCDBContext(DbContextOptions<WICCDBContext> options) : base(options) { }
+
+    public virtual DbSet<RefreshToken> RefreshTokens { get; set; }
 }
