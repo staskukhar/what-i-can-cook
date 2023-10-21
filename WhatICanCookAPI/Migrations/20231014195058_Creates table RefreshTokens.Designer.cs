81b7974234ce7dc4c167bafa7a28cc071ec1694c
staskukhar <staskuk2005@gmail.com>
2023-10-21 11:50:01 +0300

    adds migrations related to tokens table

--- WhatICanCookAPI/Migrations/20231014195058_Creates table RefreshTokens.Designer.cs
+++ WhatICanCookAPI/Migrations/20231014195058_Creates table RefreshTokens.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace WhatICanCookAPI.Migrations
 {
     [DbContext(typeof(WICCDBContext))]
-    partial class WICCDBContextModelSnapshot : ModelSnapshot
+    [Migration("20231014195058_Creates table RefreshTokens")]
+    partial class CreatestableRefreshTokens
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
